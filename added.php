68cf28c8c98b2f3232507bdc0ed47314df9ed0ad
Ujjwal Gahatraj <ujjwalgahatraj@Ujjwals-MacBook-Air.local>
2025-11-19 01:11:04 +0000

    Remove login redirect checks from multiple pages so they load without requiring login

--- added.php
+++ added.php
@@ -3,8 +3,6 @@
 # Access session.
 session_start() ;
 
-# Redirect if not logged in.
-if ( !isset( $_SESSION[ 'user_id' ] ) ) { require ( 'login_tools.php' ) ; load() ; }
 
 # Set page title and display header section.
 $page_title = 'Cart Addition' ;
--- home.php
+++ home.php
@@ -2,10 +2,7 @@
 
 # Access session.
 session_start() ; 
-
-# Redirect if not logged in.
-if ( !isset( $_SESSION[ 'user_id' ] ) ) { require ( 'login_tools.php' ) ; load() ; }
-
+#
 # Set page title and display header section.
 $page_title = 'Home' ;
 include ( 'includes/header.html' ) ;
